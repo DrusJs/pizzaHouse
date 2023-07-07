0e28309b0ce5dfc03014e00ee2d80ccd9d1f317a
DrusJs <magnojoky962@gmail.com>
2023-07-07 17:56:19 +0300

    Modal Desctop Action
    Add Modal Desctop Action

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const burgerButton = document.getElementById("burger-button");
+const modalWrappers = document.querySelectorAll(".modal-wrapper");
 const popupMenu = document.getElementById("popup-menu");
+const modalCloseButtons = document.querySelectorAll(".modal__close-button");
+
 burgerButton.addEventListener("click", ()=>{
     burgerButton.classList.toggle("active");
     if (burgerButton.classList.contains("active")) {
@@ -7,4 +10,20 @@ burgerButton.addEventListener("click", ()=>{
     } else {
         popupMenu.classList.remove("active");
     }
+});
+
+modalWrappers.forEach(el =>{
+    el.addEventListener("click", (e) => {
+        e.currentTarget.classList.remove("active");
+    });
+});
+
+modalCloseButtons.forEach(el => {
+    el.addEventListener("click", (e) => {
+        e.currentTarget.parentElement.parentElement.classList.remove("active");
+    });
+});
+
+document.getElementById("basket-button").addEventListener("click", () => {
+    document.getElementById("modal-register").classList.add("active");
 });
