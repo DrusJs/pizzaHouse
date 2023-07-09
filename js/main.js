cb3844b89190f3af6bac081e645160154e985c7a
DrusJs <magnojoky962@gmail.com>
2023-07-09 16:52:16 +0300

    Modal Auth Update
    Add auth modals

--- js/main.js
+++ js/main.js
@@ -32,16 +32,19 @@ modalCloseButtons.forEach(el => {
     });
 });
 
-// document.getElementById("basket-button").addEventListener("click", () => {
-//     document.getElementById("modal-register").classList.add("active");
-// });
 
+try{
 document.getElementById("cnstr-button").addEventListener("click", () => {
     document.getElementById("modal-constructor").classList.add("active");
 });
+} catch {}
 
-// locationBlock.addEventListener("click", () => {
-//     locationSelect.classList.toggle("active");
+// locationBlock.addEventListener("click", (e) => {
+//     if (e.target.classList.contains("location") || e.target.classList.contains("location-select-value")){
+//         locationSelect.classList.add("active");
+//     } else {
+//         locationSelect.classList.remove("active");
+//     }
 // });
 locationBlock.addEventListener("mouseover", () => {
     locationSelect.classList.add("active");
@@ -68,3 +71,70 @@ document.querySelectorAll(".menu__filter-list").forEach(el => {
         e.currentTarget.classList.add("active");
     });
 });
+
+function setActionModal() {
+    document.querySelector(".container-info__user").addEventListener("click", () => {
+        document.getElementById("modal-login").classList.add("active");
+    });
+    document.querySelector(".modal__link.login").addEventListener("click", () => {
+        document.querySelector(".modal-wrapper.active").classList.remove("active");
+        document.getElementById("modal-login").classList.add("active");
+    });
+    document.querySelector(".modal__link.register").addEventListener("click", () => {
+        document.querySelector(".modal-wrapper.active").classList.remove("active");
+        document.getElementById("modal-register").classList.add("active");
+    });
+    document.querySelector(".modal__link.remind").addEventListener("click", () => {
+        document.querySelector(".modal-wrapper.active").classList.remove("active");
+        document.getElementById("modal-remind").classList.add("active");
+    });
+    document.querySelectorAll(".reset-btn").forEach(el => {
+        el.addEventListener("click", () => {
+            document.querySelector(".modal-wrapper.active").classList.remove("active");
+        });
+    });
+}
+
+setActionModal();
+
+function setActionsCards() {
+    document.querySelectorAll(".card-pizza_cart-btn").forEach(el => {
+        el.addEventListener("click", (e) => {
+            if (e.target.classList.contains("card-pizza_cart-btn")){
+                e.currentTarget.classList.add("active");
+                document.querySelector(".header__basket").classList.remove("empty");
+                document.querySelector(".header__basket-amount").innerHTML = +document.querySelector(".header__basket-amount").innerHTML + 1;
+            }
+        });
+    });
+    document.querySelectorAll(".cart-btn__inc.plus").forEach(el => {
+        el.addEventListener("click", (e) => {
+            let multy = e.currentTarget.nextElementSibling.firstElementChild.innerHTML;
+            e.currentTarget.nextElementSibling.firstElementChild.innerHTML = +multy+1;
+        });
+    });
+    document.querySelectorAll(".cart-btn__inc.minus").forEach(el => {
+        el.addEventListener("click", (e) => {
+            let multy = e.currentTarget.nextElementSibling.nextElementSibling.firstElementChild.innerHTML;
+            if (+multy < 2){
+                let cart = +document.querySelector(".header__basket-amount").innerHTML;
+                e.currentTarget.parentElement.classList.remove("active");
+                document.querySelector(".header__basket-amount").innerHTML = cart - 1;
+                if (+document.querySelector(".header__basket-amount").innerHTML < 1) {
+                    document.querySelector(".header__basket").classList.add("empty");
+                }
+            } else {
+                e.currentTarget.nextElementSibling.nextElementSibling.firstElementChild.innerHTML = +multy-1;
+            }
+        });
+    });
+    document.querySelectorAll(".card-pizza__btn").forEach(el => {
+        el.addEventListener("click", (e) => {
+            if (e.target.classList.contains("card-pizza__btn")){
+                document.getElementById("modal-ingredients").classList.add("active");
+            }
+        });
+    });
+}
+
+setActionsCards();
